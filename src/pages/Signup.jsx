cd82108f4d599af1f0f358b831d9bafe4cdf9a56
nurullah91 <mdnurullah.ptk@gmail.com>
2023-09-29 22:53:22 +0600

    sign up done

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from 'react';
 import loginImage from '../assets/image/login.svg';
 import { useForm, useWatch } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { createUser } from '../redux/features/user/userSlice';
+import toast, { Toaster } from 'react-hot-toast';
 
 const Signup = () => {
   const { handleSubmit, register, control } = useForm();
@@ -9,6 +12,9 @@ const Signup = () => {
   const confirmPassword = useWatch({ control, name: 'confirmPassword' });
   const navigate = useNavigate();
   const [disabled, setDisabled] = useState(true);
+  const { isLoading, email, isError, error} = useSelector((state) => state.userSlice);
+  const dispatch = useDispatch();
+
 
   useEffect(() => {
     if (
@@ -24,8 +30,27 @@ const Signup = () => {
     }
   }, [password, confirmPassword]);
 
+// handle signUp error
+  useEffect( () => {
+    if(isError && error){
+      toast.error(error)
+    }
+  },[isError, error])
+
+// Navigate after sign in
+  useEffect( () => {
+    if(!isLoading && email){
+     navigate('/')
+    }
+  },[ isLoading, email])
+
   const onSubmit = ({ name, email, password }) => {
     // Email Password signup
+    dispatch(createUser({
+      name,
+      email,
+      password
+    }))
     console.log(name, email, password);
   };
 
@@ -35,6 +60,7 @@ const Signup = () => {
 
   return (
     <div className="flex max-w-7xl mx-auto h-screen items-center">
+      <Toaster />
       <div className="w-1/2">
         <img src={loginImage} className="h-full w-full" alt="" />
       </div>
